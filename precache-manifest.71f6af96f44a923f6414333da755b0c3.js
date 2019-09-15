54e56147b582bc39a136bcf9fcbd6cfc76d17794
sumitkumardey91 <sumit.kumar@mobiotics.com>
2019-09-16 00:39:22 +0530

    Updates

---
+++ precache-manifest.71f6af96f44a923f6414333da755b0c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a633279a5079fb0f376834d34aa07a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71eb7b0642ee9e821665",
+    "url": "/static/css/main.45443850.chunk.css"
+  },
+  {
+    "revision": "f43d72b2d4cf234b6766",
+    "url": "/static/js/2.b040dda1.chunk.js"
+  },
+  {
+    "revision": "71eb7b0642ee9e821665",
+    "url": "/static/js/main.7d598a86.chunk.js"
+  },
+  {
+    "revision": "815182fe12fddaaf959c",
+    "url": "/static/js/runtime~main.42a08f26.js"
+  }
+]);
